ba6eb9571345236d9baead6b25006941d2a1fa27
dianagarcial <diana_ma.garcia@uao.edu.co>
2022-02-14 21:21:29 -0500

    PRIMERA PARTE

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import './App.css';
 import { Login } from './components/Login';
 import { SignUp } from './components/SignUp';
 import { Home } from './components/Home';
+import { AuthProvider } from './context/AuthContext';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { PrivateRoute } from './components/PrivateRoute';
 
 function App() {
   return (
     <>
-      <Router >
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={SignUp} />
-        </Switch>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,50 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import { useAuth } from '../context/AuthContext';
+//import Spinner from '../spinner.svg';
 
 export const Login = () => {
-  const [error, setError] = useState(null);
+
+  const { login } = useAuth();
+  const [error, setError] = useState('');
+  //const [loading, setLoading] = useState(false);
+  //const history = useHistory();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = e => setEmail(e.target.value);
+  const handlePassword = e => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //setLoading(true);
+    try {
+      await login(email, password);
+      //setLoading(false);
+      //history.push('/');
+    } catch (error) {
+      //setLoading(false);
+      setError('Credenciales incorrectas');
+      setTimeout(() => setError(''), 1500);
+    }
+  }
+
+
   return (
     <div className='card'>
       <div className='card-header' >
-        {error && <p className='error' >{{ error }}</p>}
+        {error && <p className='error' >{error}</p>}
         <h1>Log In</h1>
       </div>
       <div className='card-body'>
-        <form>
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
+        <form onSubmit={handleSubmit}>
+          <input type='email' placeholder='Email' onChange={handleEmail} />
+          <input type='password' placeholder='Password' onChange={handlePassword} />
           <input type='submit' value='Log In' />
         </form>
+        {loading && <img src={Spinner} alt='Loading' />}
         <p>Don't have an account? <Link to='/signup'>Sign Up</Link> </p>
       </div>
 
